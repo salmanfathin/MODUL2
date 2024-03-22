5528400f477900003a7bae2af09c19eb044688c5
salmanfathin <salmanfthn@gmail.com>
2024-03-22 14:01:49 +0700

    add show status and gol

--- app/src/main/java/com/example/modul2/MainActivity.java
+++ app/src/main/java/com/example/modul2/MainActivity.java
@@ -29,15 +29,27 @@ public class MainActivity extends AppCompatActivity {
         status = (CheckBox) findViewById(R.id.menikah);
         if (status.isChecked()) {
             gajiStatus = 500000;
+            //Menampilkan Status Menikah
+            TextView outputStatus = (TextView) findViewById(R.id.outputStatus);
+            outputStatus.setText("Status Anda Menikah ");
         } else {
             gajiStatus = 0;
+            //Menampilkan Status Belum Menikah
+            TextView outputStatus = (TextView) findViewById(R.id.outputStatus);
+            outputStatus.setText("Status Anda Belum Menikah ");
         }
         golongan = (RadioGroup) findViewById(R.id.rgGolongan);
         int gol = golongan.getCheckedRadioButtonId();
         if (gol == R.id.rbGolongan1) {
             gajiGolongan = 1000000;
+            //Menampilkan Golongan 1
+            TextView outputStatus2 = (TextView) findViewById(R.id.outputStatus2);
+            outputStatus2.setText("Golongan Anda 1 ");
         } else if (gol == R.id.rbGolongan2) {
             gajiGolongan = 2000000;
+            //Menampilkan Golongan 1
+            TextView outputStatus2 = (TextView) findViewById(R.id.outputStatus2);
+            outputStatus2.setText("Golongan Anda 2 ");
         }
         ;
         int totalGaji = gajiStatus + gajiGolongan;
